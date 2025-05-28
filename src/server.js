39ebfba5b9370f52ff419a1ac27b24a744da6de6
Aaron Skriloff <69407772+yyaskriloff@users.noreply.github.com>
2025-05-29 00:48:07 +0300

    removed leaked keys (they don't work anymore)

--- src/server.js
+++ src/server.js
@@ -1,24 +1,35 @@
 import express from "express";
-const {
+import {
   setupKinde,
   protectRoute,
   getUser,
-} = require("@kinde-oss/kinde-node-express");
+  GrantType,
+} from "@kinde-oss/kinde-node-express";
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 const config = {
-  clientId: "72e83200490b41c4a0e4d54851662088",
-  issuerBaseUrl: "https://alot.kinde.com",
-  siteUrl: "http://localhost:3000",
-  secret: "FFcWSO7G5ZeXiZHbjIir5QofN2xpdcotSh6LYqaRFIldbEwkZK",
-  redirectUrl: "http://localhost:3000/kinde_callback",
+  clientId: process.env.KINDE_CLIENT_ID,
+  issuerBaseUrl: process.env.KINDE_ISSUER_BASE_URL,
+  siteUrl: process.env.KINDE_SITE_URL,
+  secret: process.env.KINDE_SECRET,
+  redirectUrl: process.env.KINDE_REDIRECT_URL,
+  scope: "openid profile email",
+  grantType: GrantType.AUTHORIZATION_CODE,
+  unAuthorisedUrl: "http://localhost:3000/unauthorised",
+  postLogoutRedirectUrl: "http://localhost:3000",
 };
 setupKinde(config, app);
 
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
 app.listen(port, () => {
