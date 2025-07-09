86515491f5ecfd4fe289f7886d884d4248db58bf
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2025-07-09 16:10:47 +0300

    add create method

--- src/routes/drive.ts
+++ src/routes/drive.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono'
-import db, { drivesTable } from '../db'
+import db, { drivesTable, region, orgsTable } from '../db'
 import { eq } from 'drizzle-orm'
 import { zValidator as validator } from '@hono/zod-validator'
-import { insertDriveSchema } from '../db'
 import { z } from 'zod/v4'
+import { clerkClient } from '../lib/clerk'
 
 const driveRoute = new Hono()
 
@@ -23,17 +23,80 @@ driveRoute.post(
   '/',
   validator(
     'json',
-    insertDriveSchema.omit({
-      id: true,
-      ownerId: true,
-      storageUsed: true,
-      storageLimit: true,
-      createdAt: true,
-      updatedAt: true
-    })
+    z
+      .object({
+        name: z.string().min(1, 'Name is required'),
+        slug: z.string().min(1, 'Slug is required'),
+        region: z.enum(region.enumValues),
+        type: z.literal('organization')
+      })
+      .or(
+        z.object({
+          region: z.enum(region.enumValues),
+          type: z.literal('personal')
+        })
+      )
   ),
   async c => {
     const user = c.get('user')
+
+    const body = c.req.valid('json')
+
+    if (body.type === 'personal') {
+      // insert drive into database
+
+      const [newDrive] = await db
+        .insert(drivesTable)
+        .values({
+          type: body.type,
+          region: body.region,
+          ownerId: user.id
+        })
+        .onConflictDoNothing()
+        .returning()
+
+      return c.json(newDrive, 201)
+    }
+
+    // create new org in clerk
+    const newOrg = await clerkClient.organizations.createOrganization({
+      name: body.name,
+      slug: body.slug
+    })
+
+    clerkClient.organizations.createOrganizationMembership({
+      organizationId: newOrg.id,
+      userId: user.clerkId,
+      role: 'owner'
+    })
+
+    //create new org in db
+    const [newOrgDb] = await db
+      .insert(orgsTable)
+      .values({
+        name: newOrg.name,
+        slug: newOrg.slug,
+        clerkId: newOrg.id
+      })
+      .onConflictDoNothing()
+      .returning()
+
+    if (!newOrgDb) {
+      return c.json({ error: 'Failed to create organization' }, 500)
+    }
+
+    const [newDrive] = await db
+      .insert(drivesTable)
+      .values({
+        type: 'organization',
+        region: body.region,
+        ownerId: newOrgDb.id
+      })
+      .onConflictDoNothing()
+      .returning()
+
+    // create drive linking org to drive
+    return c.json(newDrive, 201)
   }
 )
 
