28d0e08a5c7ed0131a93c6ff2d342b1f4603e43e
yyaskriloff <69407772+yyaskriloff@users.noreply.github.com>
2025-07-09 16:10:39 +0300

    add drive auth middleware

--- src/middlleware.ts
+++ src/middlleware.ts
@@ -1,15 +1,8 @@
-//  auth user
-//  user
-//  persinalDrives
-//  orgDrives
-// currentDrive
-
 import { createMiddleware } from 'hono/factory'
 import { getAuth } from '@hono/clerk-auth'
 import { getClerkUser, clerkClient } from './lib/clerk'
-import db, { type DbUser } from './db'
-
-const userCache = new Map<string, DbUser>()
+import db from './db'
+import { userCache, driveCache, orgCache } from './lib/cache'
 
 export const getUser = createMiddleware(async (c, next) => {
   const auth = getAuth(c)
@@ -60,6 +53,75 @@ export const getUser = createMiddleware(async (c, next) => {
   await next()
 })
 
-export const getDriveContext = createMiddleware(async (c, next) => {
-  await next()
-})
+export const getDriveContext = (...permissionsNeeded: string[]) =>
+  createMiddleware(async (c, next) => {
+    const driveId = c.req.query('id')
+    const user = c.get('user')
+
+    if (!driveId) {
+      return c.json({ error: 'Drive ID is required' }, 400)
+    }
+
+    // make sure drive exists and save it to cache
+    if (!driveCache.has(driveId)) {
+      const drive = await db.query.drivesTable.findFirst({
+        where: (drivesTable, { eq }) => eq(drivesTable.id, driveId)
+      })
+
+      if (!drive) {
+        return c.json({ error: 'Drive not found' }, 404)
+      }
+
+      driveCache.set(driveId, drive)
+    }
+
+    const drive = driveCache.get(driveId)!
+
+    //  if the drive is personal, check if the user is the owner
+    if (drive.type === 'personal') {
+      if (drive.ownerId !== user.id) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+      c.set('drive', drive)
+      return next()
+    }
+
+    // if the drive is an org, check if the user is a member of the org and has the required permissions
+    const auth = getAuth(c)!
+
+    const clerkOrgId = auth!.orgId
+
+    if (typeof clerkOrgId !== 'string') {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
+
+    if (!orgCache.has(clerkOrgId)) {
+      const org = await db.query.orgsTable.findFirst({
+        where: (orgsTable, { eq, and }) => and(eq(orgsTable.clerkId, clerkOrgId))
+      })
+
+      if (!org) {
+        return c.json({ error: 'Organization not found' }, 404)
+      }
+
+      orgCache.set(clerkOrgId, org)
+    }
+
+    const org = orgCache.get(clerkOrgId)!
+
+    if (drive.ownerId !== org.id) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
+
+    if (permissionsNeeded.length > 0) {
+      const userPermissions = auth.orgPermissions || []
+
+      const hasPermission = permissionsNeeded.every(permission => userPermissions.includes(permission))
+      if (!hasPermission) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+    }
+
+    c.set('drive', drive)
+    return next()
+  })
